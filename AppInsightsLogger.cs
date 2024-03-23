fd50b96b42f5b329ae3ed9ee9f9c6e7c680c4fad
Oscar van Tol <o.van.tol@virtualvaults.com>
2024-03-23 07:47:21 +0100

    update project info

--- AppInsightsLogger.cs
+++ AppInsightsLogger.cs
@@ -15,6 +15,7 @@ public class AppInsightsLogger : ITestLoggerWithParameters
 {
     private TelemetryClient _telemetryClient;
     private string _operationId = new Guid().ToString();
+    private bool _debug = false;
 
     public void Initialize(TestLoggerEvents events, string testRunDirectory)
     {
@@ -31,10 +32,16 @@ public class AppInsightsLogger : ITestLoggerWithParameters
             return;
         }
 
+        if (parameters.TryGetValue("Debug", out var debugValue))
+            _debug = bool.TryParse(debugValue, out var debug) && debug;
+
         _telemetryClient = new TelemetryClient(new Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration
         {
             ConnectionString = connectionstring
         });
+
+        Console.WriteLine("ApplicationInsightsConnectionString provided, telemetry will be sent.");
+        _telemetryClient.TrackTrace("Test run started", SeverityLevel.Information);
 
 
         events.DiscoveryComplete += Events_DiscoveryComplete;
@@ -45,19 +52,25 @@ public class AppInsightsLogger : ITestLoggerWithParameters
 
     private void Events_DiscoveredTests(object sender, DiscoveredTestsEventArgs e)
     {
+        _telemetryClient.TrackTrace("### Events_DiscoveredTests", SeverityLevel.Information, new Dictionary<string, string>() { { "TestCases", e.DiscoveredTestCases.Count().ToString() } });
+
         e.DiscoveredTestCases.ToList().ForEach(testCase =>
         {
+            LogDebug($"tracking trace for discovered test: {testCase.DisplayName}");
             var traceTelemetry = new TraceTelemetry();
             traceTelemetry.Message = $"Discovered test {testCase.DisplayName}";
             traceTelemetry.Timestamp = DateTime.Now;
+            traceTelemetry.SeverityLevel = SeverityLevel.Information;
             testCase.Traits.ToList().ForEach(trait =>
             {
                 traceTelemetry.Properties.Add(trait.Name, trait.Value);
+                LogDebug($"trait: {trait.Name} - {trait.Value}");
             });
             testCase.Properties.ToList().ForEach(property =>
             {
                 traceTelemetry.Properties.Add("Category", property.Category);
                 traceTelemetry.Properties.Add("Label", property.Label);
+                LogDebug($"property: category {property.Category} / label {property.Label}");
             });
             _telemetryClient.TrackTrace(traceTelemetry);
         });
@@ -65,12 +78,23 @@ public class AppInsightsLogger : ITestLoggerWithParameters
 
     private void Events_DiscoveryComplete(object sender, DiscoveryCompleteEventArgs e)
     {
+        LogDebug("### Events_DiscoveryComplete");
+        LogDebug($"TotalCount: {e.TotalCount}");
+        LogDebug($"SkippedDiscoveredSources: {e.SkippedDiscoveredSources}");
+        LogDebug($"DiscoveredExtensions: {e.DiscoveredExtensions}");
+        LogDebug($"FullyDiscoveredSources: {e.FullyDiscoveredSources}");
+
+
 
     }
 
     private void Events_TestResult(object? sender, TestResultEventArgs e)
     {
         var testResult = e.Result;
+        
+        LogDebug("### Events_TestResult");
+        LogDebug($"tracking request: {testResult.TestCase.DisplayName}");
+
         using var requestOperation = _telemetryClient.StartOperation<RequestTelemetry>(testResult.TestCase.DisplayName, _operationId);
         requestOperation.Telemetry.ResponseCode = MapResultCode(testResult.Outcome);
         requestOperation.Telemetry.Success = testResult.Outcome == TestOutcome.Passed;
@@ -86,13 +110,15 @@ public class AppInsightsLogger : ITestLoggerWithParameters
             var exceptionTelemetry = new ExceptionTelemetry();
             exceptionTelemetry.Exception = new Exception(testResult.ErrorMessage);
             exceptionTelemetry.ProblemId = testResult.ErrorMessage;
-            exceptionTelemetry.SeverityLevel = SeverityLevel.Error;
+            exceptionTelemetry.SeverityLevel = SeverityLevel.Critical;
             exceptionTelemetry.Timestamp = testResult.StartTime;
             exceptionTelemetry.Message = testResult.ErrorStackTrace;
             _telemetryClient.TrackException(exceptionTelemetry);
+            LogDebug($"tracking exception: {testResult.ErrorMessage}");
         }
 
         _telemetryClient.StopOperation(requestOperation);
+       
     }
 
     private string MapResultCode(TestOutcome outcome)
@@ -109,5 +135,12 @@ public class AppInsightsLogger : ITestLoggerWithParameters
     private void Events_TestRunComplete(object? sender, TestRunCompleteEventArgs e)
     {
         _telemetryClient.Flush();
+        Console.WriteLine("Test run completed, flushing telemetry.");
+    }
+
+    private void LogDebug(string message)
+    {
+        if (_debug)
+            Console.WriteLine(message);
     }
 }
